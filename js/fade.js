2043603e35c4272d6d34e76b89c046f0a6c2b3d2
Koke_Cacao <1412042720@qq.com>
2018-07-11 01:30:55 -0400

    :book:first commit, add wonderful css and fading js

---
+++ js/fade.js
@@ -0,0 +1,37 @@
+// $(window).scroll(function () {
+//     console.log($(window).scrollTop());
+//     var topDivHeight = $(".preset").height();
+//     // var topDivHeight = 0;
+//     var viewPortSize = $(window).height();
+
+//     var triggerAt = 150;
+//     var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
+
+//     if ($(window).scrollTop() >= triggerHeight) {
+//         $('.hide').css('visibility', 'visible').hide().fadeIn();
+//         $(this).off('scroll');
+//     }
+// });
+
+// $("div#foo").fadeIn("fast",function(){
+//       $("div#bar").fadeIn("fast", function(){
+//            // etc.
+//       });
+//    });
+
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
