7a7831c89a52c40b45086e6e007a020a054b8f1b
Koke_Cacao <1412042720@qq.com>
2018-07-11 02:11:14 -0400

    :bug: fix fading speed and position

--- js/fade.js
+++ js/fade.js
@@ -25,12 +25,13 @@ $(window).on("load",function() {
     $(".fade").each(function() {
       /* Check the location of each desired element */
       var objectBottom = $(this).offset().top + $(this).outerHeight();
+      var objectTop = $(this).offset().top;
 
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      if (objectTop +15 < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(800,1);}
       } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        if ($(this).css("opacity")==1) {$(this).fadeTo(800,0);}
       }
     });
   }).scroll(); //invoke scroll-handler on page-load
